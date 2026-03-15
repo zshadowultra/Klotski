3ce54aa76a3aaff6e204a45b98a1923d396ff7d8
Z-SHADOW ULTRA <88608810+zshadowultra@users.noreply.github.com>
2026-03-15 19:15:11 +0530

    feat: Relocate sound files and update paths
    Moves sound effect files from `/audio` to `/sounds` and updates the `src` paths in `soundManager.ts` to reflect the new location. This organizes audio assets and ensures correct loading.

--- src/soundManager.ts
+++ src/soundManager.ts
@@ -1,9 +1,9 @@
 import { Howl, Howler } from 'howler';
 
 const sounds = {
-  select: new Howl({ src: ['/audio/click1.ogg'], volume: 0.15 }),
-  move: new Howl({ src: ['/audio/switch1.ogg'], volume: 0.25 }),
-  win: new Howl({ src: ['/audio/switch33.ogg'], volume: 0.4 }),
+  select: new Howl({ src: ['/sounds/select.ogg'], volume: 0.15 }),
+  move: new Howl({ src: ['/sounds/move.ogg'], volume: 0.25 }),
+  win: new Howl({ src: ['/sounds/win.ogg'], volume: 0.4 }),
 };
 
 export const playSound = async (soundName: keyof typeof sounds) => {
